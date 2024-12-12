26ebea8424a0811452c9f282027604e3326a233f
Yan Pan <yan@yan.fi>
2024-12-12 14:01:32 +0000

    Networking done

---
+++ dyndns/entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+while true; do
+  echo "$(date -u '+%Y-%m-%d %H:%M:%S')"
+  
+  external_ip1=$(curl -s https://api.ipify.org)
+  echo "[ipify] $external_ip1"
+
+  external_ip2=$(curl -s https://ipv4.svc.joker.com/nic/myip)
+  echo "[joker] $external_ip2"
+
+  if [ ${#external_ip1} -ge ${#external_ip2} ]; then
+    external_ip=$external_ip1
+  else
+    external_ip=$external_ip2
+  fi
+
+  echo "> $(curl -s "https://svc.joker.com/nic/update?username=$U1&password=$P1&myip=$external_ip&hostname=$D1") $D1"
+  echo "> $(curl -s "https://svc.joker.com/nic/update?username=$U2&password=$P2&myip=$external_ip&hostname=$D2") $D2"
+
+  sleep 900
+done
