a2202c4738832b2a224222542c7b220c79be017b
Yan Pan <yan@yan.fi>
2026-02-10 15:43:06 +0200

    add /pdf tools

--- poll/manual_monthly.sh
+++ poll/manual_monthly.sh
@@ -14,6 +14,9 @@ ffmpeg -f concat -safe 0 -i files.txt \
 
 rm files.txt
 
+python /app/helper_upload.py --source-file ${output_file}.mp4 --bucket-name yyyCam --dest-path "monthly"
 # faster & scratch
-ffmpeg -i ${output_file}.mp4 -vf "setpts=0.2*PTS" -r 30 -crf 28 ${output_file}f.mp4
-ffmpeg -i ${output_file}.mp4 -vf "scale=-2:1620" -r 30 -crf 35 ${output_file}s.mp4
+ffmpeg -i ${output_file}.mp4 -vf "setpts=0.2*PTS,scale=-2:1620" -r 30 -crf 35 ${output_file}f.mp4
+
+rm ${output_file}.mp4
+mv ${output_file}f.mp4 ${output_file}.mp4
