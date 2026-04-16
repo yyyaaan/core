5f6f6047bb7e0ea0f291e9823c6ba4d5334a7f54
Yan Pan <yan@yan.fi>
2026-04-16 13:22:35 +0000

    hotfix on path

--- apps/bash-poller/src/poller/job_timelapse.py
+++ apps/bash-poller/src/poller/job_timelapse.py
@@ -1,31 +1,63 @@
 from argparse import ArgumentParser
-from datetime import datetime
-from requests import post
+from datetime import datetime, timedelta
+from time import sleep
+
+from requests import get, post
+
+print("Started")
+
 
 parser = ArgumentParser(prog="Timelapse Preparation")
 parser.add_argument("--hours", type=int, default=24)
 parser.add_argument("--offset", type=int, default=0)
-
+parser.add_argument("--limit-polling-hours", type=int, default=9)
 args = parser.parse_args()
 
-print(f"@{datetime.now():%b%d %H:%M:%S}")
 
 # always relative to yesterday last hour
 now = datetime.now()
+end_polling_time = now + timedelta(hours=args.limit_polling_hours)
+
 yesterday_end = datetime(now.year, now.month, now.day)
 ts = int(yesterday_end.timestamp()) - args.offset * 3600
-ts0 = ts -  args.hours * 3600
+ts0 = ts - args.hours * 3600
 
 r = post(
     url=f"http://192.168.4.81:5000/api/export/yard/start/{ts0}/end/{ts}",
     json={
-        "playback": "timelapse_25x", # actual speed set in frigate config
-        "name": f"day90sec_{datetime.fromtimestamp(ts0):%Y%m%d}.mp4"
-    }
+        "playback": "timelapse_25x",  # actual speed set in frigate config
+        "name": f"day90sec_{datetime.fromtimestamp(ts0):%Y%m%d}.mp4",
+    },
 )
 
 print(
-    r.status_code, 
+    r.status_code,
     f"{datetime.fromtimestamp(ts0):%b%dT%H:%M:%S}--{datetime.fromtimestamp(ts):%b%dT%H:%M:%S}",
-    r.text
+    r.text,
 )
+
+
+# polling for completion
+export_id = r.json()["export_id"]
+
+
+def check_completion(export_id: str) -> bool:
+    """poll for completion (the opposite of in progress)"""
+    r_status = get(url="http://192.168.4.81:5000/api/exports")
+    for item in r_status.json():
+        if item["id"] == export_id:
+            return not item["in_progress"]
+    raise Exception("Export ID not found in polling endpoint")
+
+
+export_completed = False
+while not export_completed:
+    export_completed = check_completion(export_id)
+    print("waiting...")
+    if datetime.now() > end_polling_time:
+        raise Exception("Polling ends due to time limit. Export not completed")
+    sleep(300)
+
+print("Ready!")
+
+# python job_timelapse.py --hours 1 --limit-polling-hours 1
--- infra/credentials/env.md
+++ infra/credentials/env.md
@@ -1,3 +1,11 @@
-Environment variables deliver only via kubernetes secrets/configmaps.
+# Environment variables are directly delivered to Kubernetes
 
-This folder shall put `theme.env` by the nature of secrets, see Taskfile.yaml `infra:secrets:` task for secrets loading.
+This folder shall put `theme.env` by the nature of secrets; __Taskfile.yaml `infra:secrets:` task loads them__.
+
+```
+# from consolidated file
+for f in *.env; do echo "### File: $f ###"; cat "$f"; echo -e "\n"; done > .env.combined
+
+# concatenate envs
+awk '/^### File: / { out=$3; next } { if (out) print > out }' .env.combined
+```
