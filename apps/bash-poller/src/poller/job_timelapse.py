b0b0401b503f657fa4165953a507c11a322ef78a
Yan Pan <yan@yan.fi>
2026-04-17 13:14:10 +0300

    Feature/better polling and cronjob (#5)
    * ffmpeg cronjob
    
    * Refine application charts
    
    * add debug pod
    
    * directory must be existing
    
    * remove DirectoryOrCreate: Directory must exist

--- apps/bash-poller/src/poller/job_timelapse.py
+++ apps/bash-poller/src/poller/job_timelapse.py
@@ -1,32 +1,28 @@
-from argparse import ArgumentParser
+# this file is moved into kubernetes manifest start point
 from datetime import datetime, timedelta
 from time import sleep
-
 from requests import get, post
 
+offset = 0 # yesterday
+hours = 24 # a whole day
+name_prefix = "day90sec"
+max_polling_hours = 8
+polling_interval_seconds = 300
+frigate_url = "http://frigate-service.home-internal.svc.cluster.local:5000"
+
 print("Started")
-
-
-parser = ArgumentParser(prog="Timelapse Preparation")
-parser.add_argument("--hours", type=int, default=24)
-parser.add_argument("--offset", type=int, default=0)
-parser.add_argument("--limit-polling-hours", type=int, default=9)
-args = parser.parse_args()
-
-
-# always relative to yesterday last hour
 now = datetime.now()
-end_polling_time = now + timedelta(hours=args.limit_polling_hours)
+end_polling_time = now + timedelta(hours=max_polling_hours)
 
 yesterday_end = datetime(now.year, now.month, now.day)
-ts = int(yesterday_end.timestamp()) - args.offset * 3600
-ts0 = ts - args.hours * 3600
+ts = int(yesterday_end.timestamp()) - offset * 3600
+ts0 = ts - hours * 3600
 
 r = post(
-    url=f"http://frigate-service.home-internal.svc.cluster.local:5000/api/export/yard/start/{ts0}/end/{ts}",
+    url=f"{frigate_url}/api/export/yard/start/{ts0}/end/{ts}",
     json={
         "playback": "timelapse_25x",  # actual speed set in frigate config
-        "name": f"day90sec_{datetime.fromtimestamp(ts0):%Y%m%d}.mp4",
+        "name": f"{name_prefix}_{datetime.fromtimestamp(ts0):%Y%m%d}.mp4",
     },
 )
 
@@ -34,16 +30,16 @@ print(
     r.status_code,
     f"{datetime.fromtimestamp(ts0):%b%dT%H:%M:%S}--{datetime.fromtimestamp(ts):%b%dT%H:%M:%S}",
     r.text,
+    flush=True,
 )
 
 
 # polling for completion
 export_id = r.json()["export_id"]
 
-
 def check_completion(export_id: str) -> bool:
     """poll for completion (the opposite of in progress)"""
-    r_status = get(url="http://frigate-service.home-internal.svc.cluster.local:5000/api/exports")
+    r_status = get(url=f"{frigate_url}/api/exports")
     for item in r_status.json():
         if item["id"] == export_id:
             return not item["in_progress"]
@@ -52,12 +48,11 @@ def check_completion(export_id: str) -> bool:
 
 export_completed = False
 while not export_completed:
+    sleep(10)
     export_completed = check_completion(export_id)
-    print("waiting...")
+    print("waiting...", flush=True)
     if datetime.now() > end_polling_time:
         raise Exception("Polling ends due to time limit. Export not completed")
-    sleep(300)
+    sleep(polling_interval_seconds)
 
 print("Ready!")
-
-# python job_timelapse.py --hours 1 --limit-polling-hours 1
